2e79b5d560126e10cf560518239a254b220a9b65
Glenn Fitzpatrick <glenn@glennfitzpatrick.com>
2021-01-25 15:47:33 -0500

    Show all qualities in filter dropdown

--- app/main/routes.py
+++ app/main/routes.py
@@ -1314,10 +1314,6 @@ def movie_shopping():
 
     qualities = (
         db.session.query(RefQuality.id, RefQuality.quality_title)
-        .join(File, (File.quality_id == RefQuality.id))
-        .distinct()
-        .filter(File.movie_id != None)
-        .filter(File.feature_type_id == None)
         .order_by(RefQuality.preference.asc())
         .all()
     )
